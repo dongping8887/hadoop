ebf9831d7cc85fe9912900e175bd0720ff8933fa
dongping_8887 <dongping_8887@126.com>
2019-03-22 17:43:59 +0800

    1.添加日志

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/api/records/impl/pb/ContainerPBImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/api/records/impl/pb/ContainerPBImpl.java
@@ -18,25 +18,19 @@
 
 package org.apache.hadoop.yarn.api.records.impl.pb;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.classification.InterfaceAudience.Private;
 import org.apache.hadoop.classification.InterfaceStability.Unstable;
 import org.apache.hadoop.security.proto.SecurityProtos.TokenProto;
-import org.apache.hadoop.yarn.api.records.Container;
-import org.apache.hadoop.yarn.api.records.ContainerId;
-import org.apache.hadoop.yarn.api.records.NodeId;
-import org.apache.hadoop.yarn.api.records.Priority;
-import org.apache.hadoop.yarn.api.records.Resource;
-import org.apache.hadoop.yarn.api.records.Token;
-import org.apache.hadoop.yarn.proto.YarnProtos.ContainerIdProto;
-import org.apache.hadoop.yarn.proto.YarnProtos.ContainerProto;
-import org.apache.hadoop.yarn.proto.YarnProtos.ContainerProtoOrBuilder;
-import org.apache.hadoop.yarn.proto.YarnProtos.NodeIdProto;
-import org.apache.hadoop.yarn.proto.YarnProtos.PriorityProto;
-import org.apache.hadoop.yarn.proto.YarnProtos.ResourceProto;
+import org.apache.hadoop.yarn.api.records.*;
+import org.apache.hadoop.yarn.proto.YarnProtos.*;
 
 @Private
 @Unstable
 public class ContainerPBImpl extends Container {
+
+  private static final Log LOG = LogFactory.getLog(ContainerPBImpl.class);
 
   ContainerProto proto = ContainerProto.getDefaultInstance();
   ContainerProto.Builder builder = null;
@@ -171,7 +165,11 @@ public class ContainerPBImpl extends Container {
     if (!p.hasNodeHttpAddress()) {
       return null;
     }
-    return (p.getNodeHttpAddress());
+    //modify by dongping 20190322 end
+    String nodeHttpAddress = (p.getNodeHttpAddress());
+    LOG.debug("nodeHttpAddress: " + nodeHttpAddress);
+    return nodeHttpAddress;
+    //modify by dongping 20190322 end
   }
 
   @Override
